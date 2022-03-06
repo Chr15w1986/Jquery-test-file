08fab9958007404d4f5f6ebb01a4751a047a2b65
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-03-06 19:26:27 +0000

    repace index.html with Cards-jquery.html(error during testing with jest)

--- scripts/test/button.test.js
+++ scripts/test/button.test.js
@@ -7,7 +7,7 @@ const buttonClick = require("../button");
 
 beforeAll(() => {
     let fs = require("fs");
-    let fileContents = fs.readFileSync("index.html", "utf-8");
+    let fileContents = fs.readFileSync("Cards-jquery.html", "utf-8");
     document.open();
     document.write(fileContents);
     document.close();
@@ -18,5 +18,8 @@ describe("DOM tests", () => {
     test("expects p content to change", () => {
         buttonClick();
         expect(document.getElementById("par").innerHTML).toEqual("You clicked!");
+    });
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
     });
 });
